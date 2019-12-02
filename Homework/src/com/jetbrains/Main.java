1d072ff3dac2a344a7bf295c41d9927f096328c5
Lucian Rauca <58304728+LuciRauca@users.noreply.github.com>
2019-12-02 09:13:53 +0200

    Add files via upload

---
+++ Homework/src/com/jetbrains/Main.java
@@ -0,0 +1,13 @@
+package com.jetbrains;
+import org.joda.time.DateTime;
+
+import java.io.Console;
+import java.io.Writer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DateTime date = new DateTime();
+        System.out.println(date.toString());
+    }
+}
